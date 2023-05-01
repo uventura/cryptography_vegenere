08e9de0460d62b788d51ea141597abb2338aa942
UVentura <ualivent@gmail.com>
2023-05-01 18:49:03 -0300

    Improving Vigenere Attack

--- lib/vigenere_attack.py
+++ lib/vigenere_attack.py
@@ -1,10 +1,14 @@
 from .vigenere import Vigenere
 
 class VigenereAttack:
-    def __init__(self, attacks_output = "attack_output.txt"):
-        self.attacks_output = attacks_output
+    def __init__(self):
         self.vigenere = Vigenere()
         self.language_freqs = self.get_language_frequencies()
+
+    def attack_key_size(self, msg):
+        coincidences = self.get_coincidences_shift(msg)
+        coincidences = sorted(coincidences, key = self.sort_tuple, reverse = True)
+        return self.get_common_divisors(coincidences[:20])
 
     def attack_key(self, cypher_txt, min_key_size = 2, max_key_size = 100, language = "en"):
         encrypt = self.vigenere.normalize_message(cypher_txt)
@@ -20,6 +24,17 @@ class VigenereAttack:
             guessed_keys.append(founded_key)
 
         return guessed_keys
+
+    def get_coincidences_shift(self, msg):
+        msg = "".join(msg.split())
+
+        distribution_shift = []
+        for shift in range(1, len(msg)):
+            shifted_msg = self.shift_sentence(msg, shift)
+            amount = self.amount_equal_letters(msg[shift:], shifted_msg[shift:])
+            distribution_shift.append((shift, amount))
+
+        return distribution_shift
 
     def get_best_probabilistic_letter(self, crypted_txt, group, language):
         freq = self.get_group_frequency(crypted_txt, group)
@@ -98,6 +113,25 @@ class VigenereAttack:
             frequency[ord(letter)-ord('a')] = group[letter] / len(msg)
         return frequency
 
+    def amount_equal_letters(self, msg1, msg2):
+        equal = 0
+        if len(msg1) != len(msg2):
+            return -1
+
+        msg1 = "".join(msg1.split())
+        msg2 = "".join(msg2.split())
+
+        for index in range(len(msg1)):
+            equal += int(msg1[index] == msg2[index])
+
+        return equal
+
+    def shift_sentence(self, msg, shift_amount):
+        shifted_msg = ['*' for i in range(len(msg))]
+        for index, letter in enumerate(msg):
+            shifted_msg[(index + shift_amount)%len(msg)] = letter
+        return ''.join(shifted_msg)
+
     def dot_prod(self, v1, v2):
         if len(v1) != len(v2):
             return -1
@@ -108,4 +142,21 @@ class VigenereAttack:
             prod += v1[i] * v2[i]
 
         return prod
+
+    def sort_tuple(self, element):
+        return element[1]
+
+    def get_common_divisors(self, best_frequences):
+        letter_positions = [element[0] for element in best_frequences]
+        max_value = max(letter_positions)
+        divisors = {}
+
+        for i in range(2, max_value):
+            count = 0
+            for element in letter_positions:
+                if element % i == 0:
+                    count += 1
+            if count > 2:
+                divisors[i] = count
+        return divisors
 
